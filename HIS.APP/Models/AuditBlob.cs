acf96eec6ed4e2bd55f398cdd91e834696c4082c
Administrator <Administrator@LAPTOP-AMCHSKBP>
2024-11-29 23:45:06 +0530

    Added new properties to class

--- HIS.APP/Models/AuditBlob.cs
+++ HIS.APP/Models/AuditBlob.cs
@@ -15,6 +15,18 @@ namespace HIS.APP.Models
         public string PatientId { get; set; }
         public Guid MessageGUID { get; set; }
         public string MessageTextOrRawData { get; set; }
+
+        public DateTime CreatedDate { get; set; } 
+        public DateTime? ModifiedDate { get; set; } 
+        public string CreatedBy { get; set; } 
+        public string ModifiedBy { get; set; } 
+        public string SourceSystem { get; set; } 
+        public string MessageType { get; set; } 
+        public string Status { get; set; }
+        public int RetryCount { get; set; } 
+        public string ErrorDetails { get; set; } 
+        public bool IsArchived { get; set; } 
+        public string Comments { get; set; }
     }
 }
 #endregion
